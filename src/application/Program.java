4aad6d0cf16a2aae7f6bb7a3244798d63b040802
aguiiarlucas <lucascostaaguiar.com>
2020-12-17 16:48:38 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -10,7 +13,11 @@ public class Program {
 		Department obj = new Department(1, "Books ");
 		
 		System.out.println(obj);
+		
+		Seller seller = new Seller("Lucas", "lucas@hotmail.com", 21, new Date(), 3000.0	, obj);
 
+		
+		System.out.println(seller);
 	}
 
 }
